a777a217da0868ec37d847c778835f98932647f8
Daniela Brenes <danibreo6@gmail.com>
2021-01-03 19:22:53 -0600

    Create models

---
+++ Server/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Models
+{
+    [Table("Users", Schema = "OdysseyMusicDB")]
+    public partial class Users
+    {
+        [Key]
+        [Column("id_user")]
+        public int IdUser { get; set; }
+        [Column("name_user")]
+        [StringLength(255)]
+        public string NameUser { get; set; }
+        [Column("email_user")]
+        [StringLength(255)]
+        public string EmailUser { get; set; }
+    }
+}
