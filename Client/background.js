9e116910251c804d6504cd0cb8cdb2b36994f797
varaya64 <v.valentina@estudiantec.cr>
2021-01-28 23:19:06 -0600

    Update background.js
    Fetch update

--- Client/background.js
+++ Client/background.js
@@ -4,20 +4,41 @@
   // Manejar la UI por componentes
 
   // Evento del omnibox
-  chrome.omnibox.onInputChanged.addListener(async function(text) {
-	  console.log(text);
+chrome.omnibox.onInputChanged.addListener(async function(text) {
+	console.log(text);
     alert(text + '"')
     let data = {
       search: text
     }
-    fetch(`${URL}songs/search/` + text, {
+    let songData = await fetch(`${URL}songs/search/` + text, {
       method: 'GET',
       headers: {"Content-type": "application/json"}
     })
     .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err));
+    .then(json => json)
+    for (track in songData){
+      suggest([{content: songData[track].songName + " " + songData[track].artistName, description: songData[track].songName}])
+    }
     });
+
+chrome.omnibox.onInputEntered.addListener(async function(text, disposition){
+  let myResponse = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${text}&key=AIzaSyB61Hqd0S1jfbqMuuFHMU8ojp3O8gEry9k`, {
+            method: 'GET',
+            headers: {
+                "Content-type": "application/json"
+    }})
+      .then(response => response.json())
+      .then(json => json)
+      console.log(myResponse.items[0].id.videoId)
+      await sendYoutubeLink(myResponse.items[0].id.videoId)
+})
+
+
+async function sendYoutubeLink(message){
+    chrome.runtime.sendMessage({
+        msg: message,
+    });
+}    
 
 // Hacer user login
 chrome.identity.getProfileUserInfo(async function(info) { 
@@ -28,7 +49,6 @@ chrome.identity.getProfileUserInfo(async function(info) {
   let userEmail ={
     emailJ: email
   }
-
   // POST
   fetch(`${URL}users`, {
     method: 'POST',
